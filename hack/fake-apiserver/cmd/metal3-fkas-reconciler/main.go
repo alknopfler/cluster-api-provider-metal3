792943ca3ae50581671f5647746cfa8c5af06998
Alberto Morgante Medina <alberto.morgante@suse.com>
2025-01-29 15:05:00 +0100

    change the uuid

--- hack/fake-apiserver/cmd/metal3-fkas-reconciler/main.go
+++ hack/fake-apiserver/cmd/metal3-fkas-reconciler/main.go
@@ -137,7 +137,8 @@ func main() {
 				return reconcile.Result{}, err
 			}
 
-			providerID := m3m.Spec.ProviderID
+			//providerID := m3m.Spec.ProviderID
+			providerID := "metal3://" + bmh.ObjectMeta.UID
 			setupLog.Info("----> VALUE providerID", "providerID", providerID)
 			url := "http://localhost:3333/updateNode"
 			requestData := map[string]interface{}{
